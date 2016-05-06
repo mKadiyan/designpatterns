b46028bcc84eb1b8cdd9d3cb56d2162ae15cdab0
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2016-05-06 12:19:57 +0530

    number to words

---
+++ src/algorithm/testFrameWork/NumberToWords.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) Mehar  2016 ALL RIGHTS RESERVED.
+ *
+ * 
+ */
+
+package algorithm.testFrameWork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberToWords {
+	private static Map<Integer, String> digitPlaces = new HashMap<>();
+	static
+	{
+		digitPlaces.put(3, "Hundred");
+		digitPlaces.put(4, "Thousand");
+		digitPlaces.put(6, "Lakh");
+	}
+	
+	private static Map<Integer, String> numberWords = new HashMap<>();
+	static {
+		numberWords.put(1, "One");
+		numberWords.put(2, "Two");
+		numberWords.put(3, "Three");
+		numberWords.put(4, "Four");
+		numberWords.put(5, "Five");
+		numberWords.put(6, "Six");
+		numberWords.put(7, "Seven");
+		numberWords.put(8, "Eight");
+		numberWords.put(9, "Nine");
+		numberWords.put(10, "Ten");
+		numberWords.put(11, "Eleven");
+		numberWords.put(12, "Twelve");
+		numberWords.put(13, "Thirteen");
+		numberWords.put(14, "Fourteen");
+		numberWords.put(15, "Fifteen");
+		numberWords.put(16, "Sixteen");
+		numberWords.put(17, "Seventeen");
+		numberWords.put(18, "Eighteen");
+		numberWords.put(19, "Nineteen");
+		numberWords.put(20, "Twenty");
+		numberWords.put(30, "Thirty");
+		numberWords.put(40, "Fourty");
+		numberWords.put(50, "Fifty");
+		numberWords.put(60, "Sixty");
+		numberWords.put(70, "Seventy");
+		numberWords.put(80, "Eighty");
+		numberWords.put(90, "Ninty");
+		
+	}
+	
+	public  String getWordFromNumber(int number) {
+		String valueOf = String.valueOf(number);
+		int i=valueOf.length();
+		int length =i;
+		StringBuilder sb = new StringBuilder();
+		if(numberWords.containsKey(number))
+			sb.append(numberWords.get(number));
+		else
+		{
+			while (i > 0) {
+				
+				if(i == 7 || i== 5 || i==2)
+				{
+					String valueOf1 = String.valueOf(valueOf.charAt(length - i));
+					String valueOf2 = String.valueOf(valueOf.charAt(length - i+ 1));
+					int num = Integer.valueOf(valueOf1+valueOf2);
+					if(numberWords.containsKey(num))
+					{
+						sb.append(numberWords.get(num) +" ");
+					}
+					else
+					{
+						Integer int1 = Integer.valueOf(valueOf1);
+						Integer int2 = Integer.valueOf(valueOf2);
+						if(numberWords.containsKey(int1))
+						{
+							sb.append(numberWords.get(int1*10)+" ");
+						}
+						if(numberWords.containsKey(int2))
+						{
+							sb.append(numberWords.get(int2)+" ");
+						}
+					}
+					if(digitPlaces.containsKey(i-1))
+					sb.append(digitPlaces.get(i-1) + " ");
+					i = i-2;
+					
+				}
+				else
+				{
+					String valueOf2 = String.valueOf(valueOf.charAt(length - i));
+					Integer int1 = Integer.valueOf(valueOf2);
+					if(numberWords.containsKey(int1))
+					{
+						sb.append(numberWords.get(int1)+" ");
+						if(digitPlaces.containsKey(i))
+							sb.append(digitPlaces.get(i) + " ");
+					}
+					i = i-1;
+				}
+			}
+		}
+		
+		return sb.toString();
+	}
+}
+
+/*
+ * Copyright (c) Mehar 2016 ALL RIGHTS RESERVED
+ *
+ */
---
+++ src/algorithm/testFrameWork/NumberToWordsTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Mehar  2016 ALL RIGHTS RESERVED.
+ *
+ * 
+ */
+
+package algorithm.testFrameWork;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class NumberToWordsTest {
+
+	@Test
+	@Parameters({"1,One","2,Two","3,Three","4,Four","5,Five","6,Six","7,Seven","8,Eight"
+		,"9,Nine","10,Ten","11,Eleven","12,Twelve","13,Thirteen","14,Fourteen",
+		"15,Fifteen","16,Sixteen","17,Seventeen","18,Eighteen","19,Nineteen","20,Twenty"
+		,"30,Thirty","40,Fourty","50,Fifty","60,Sixty","70,Seventy","80,Eighty","90,Ninty","90000,Ninty Thousand"})
+	public void testName(int number, String inWords) throws Exception {
+		String wordFromNumber = new NumberToWords().getWordFromNumber(number);
+		assertEquals(inWords, wordFromNumber.trim());
+	}
+	
+	@Test
+	@Parameters({"9000,Nine Thousand","21,Twenty One","210,Two Hundred Ten"})
+	public void testName1(int number, String inWords) throws Exception {
+		String wordFromNumber = new NumberToWords().getWordFromNumber(number);
+		assertEquals(inWords, wordFromNumber.trim());
+	}
+	
+	@Test
+	@Parameters({"11500,Eleven Thousand Five Hundred","1234,One Thousand Two Hundred Thirty Four","101, One Hundred One","10001,Ten Thousand One"})
+	public void testName3(int number, String inWords) throws Exception {
+		String wordFromNumber = new NumberToWords().getWordFromNumber(number);
+		assertEquals(inWords, wordFromNumber.trim());
+	}
+}
+
+
+/*
+ * Copyright (c) Mehar 2016 ALL RIGHTS RESERVED
+ *
+ */
